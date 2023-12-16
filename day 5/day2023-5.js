1fe77b1ddc6c98a7c4e5aa7c1470a40904346f37
Ferdinand Motzkus <me@motzkus.dev>
2023-12-16 14:06:54 +0000

    feat(part2): added working solution with test input

--- day 5/day2023-5.js
+++ day 5/day2023-5.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-fs.readFile("./day 5/input.txt", (err, data) => {
+fs.readFile("./day 5/test_input.txt", (err, data) => {
   const inputLines = data.toString().split("\n");
   numberRegex = /\d+/g;
 
@@ -46,6 +46,7 @@ fs.readFile("./day 5/input.txt", (err, data) => {
   }
 
   part1(almanach);
+  part2(almanach);
 
   function addToAlmanach() {
     const mergedMap = tempMapArr;
@@ -72,10 +73,33 @@ function part1(almanach) {
     if (location < startLocation) startLocation = location;
   });
 
-  console.log("start location:", startLocation);
+  console.log("start location (part1):", startLocation);
 }
 
-function part2(inputLines) {}
+function part2(almanach) {
+  const seedRangeInput = almanach.get("seeds")[0];
+  const seedGroups = [];
+  for (let index = 0; index < seedRangeInput.length; index += 2) {
+    const seedStart = seedRangeInput[index];
+    const seedRange = seedRangeInput[index + 1];
+
+    const seedGroup = new MappedRange(seedStart, seedStart, seedRange);
+    seedGroups.push(seedGroup);
+  }
+
+  let startLocation = Number.POSITIVE_INFINITY;
+  seedGroups.forEach((seedGroup) => {
+    let seed = seedGroup.start;
+
+    while (seed < seedGroup.end) {
+      let location = getLocation(seed, almanach);
+      if (location < startLocation) startLocation = location;
+      seed++;
+    }
+  });
+
+  console.log("start location (part2):", startLocation);
+}
 
 function parseSeeds(inputString) {
   const seeds = parseWithRegex(inputString, numberRegex);
@@ -139,6 +163,10 @@ class MappedRange {
   range = 0;
 
   offset = 0;
+
+  start = undefined;
+  end = undefined;
+
   /**
    *
    * @param {Number} src
@@ -151,12 +179,15 @@ class MappedRange {
     this.range = range;
 
     this.offset = dest - src;
+
+    this.start = src;
+    this.end = src + range - 1;
   }
 
   itemInRange(srcNumber) {
     let result = false;
-    const rangeStart = this.srcStart;
-    const rangeEnd = this.srcStart + this.range - 1;
+    const rangeStart = this.start;
+    const rangeEnd = this.end;
     if (srcNumber >= rangeStart && srcNumber <= rangeEnd) result = true;
     return result;
   }
